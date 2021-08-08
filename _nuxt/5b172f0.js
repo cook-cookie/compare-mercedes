3370a0131f7f228b2ac70716952e9738164143fc
cook-cookie <cook-cookie@users.noreply.github.com>
2021-08-08 09:18:17 +0000

    deploy: 1d40e6cbb958970a1b7fbc481857d5a0c457e6fe

--- _nuxt/5b172f0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{286:function(e,n,t){"use strict";t.r(n);var l=t(8).default.extend({}),r=t(62),component=Object(r.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Car List")])}),[],!1,null,null,null);n.default=component.exports}}]);
