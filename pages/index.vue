1d40e6cbb958970a1b7fbc481857d5a0c457e6fe
omachi <omachi@fenrir.co.jp>
2021-08-08 18:16:36 +0900

    update: 内容を書き換え

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <Tutorial/>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <h1 class="display-2">Compare Mercedes</h1>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          メルセデス車比較用サイト
+        </h2>
+        <p class="mt-3 text-gray-600">
+          工事中。メルセデス車の諸元比較用サイトを作成予定<br>
+        </p>
+      </div>
+      <div class="flex justify-center pt-4 space-x-2">
+        <a href="https://www.mercedes-benz.co.jp/passengercars.html">
+          メルセデス・ベンツ日本 オフィシャルサイト
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
